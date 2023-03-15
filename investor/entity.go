411944112c171a8e576f37e349d81173259fc079
ahhzaky <siahmadzaky@gmail.com>
2023-03-15 22:12:43 +0700

    :memo: fix entity

--- investor/entity.go
+++ investor/entity.go
@@ -1,1 +1,18 @@
 package investor
+
+import (
+	"time"
+)
+
+type User struct {
+	ID             int
+	Name           string
+	Email          string
+	Phone          int
+	PasswordHash   string
+	AvatarFileName string
+	Role           string
+	Token          string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+}
