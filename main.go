9eee00d2a48b358104e5cf188eccdf8064e26e1c
ahhzaky <siahmadzaky@gmail.com>
2023-03-15 15:55:37 +0700

    clear connection to db

--- main.go
+++ main.go
@@ -6,31 +6,34 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	mustGetenv := func(k string) string {
-		v := os.Getenv(k)
-		if v == "" {
-			log.Fatalf("Fatal Error in connect_tcp.go: %s environment variable not set.", k)
-		}
-		return v
+	// initial connected db
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
+
 	var (
-		dbUser    = mustGetenv("DB_USER")       // e.g. 'my-db-user'
-		dbPwd     = mustGetenv("DB_PASS")       // e.g. 'my-db-password'
-		dbName    = mustGetenv("DB_NAME")       // e.g. 'my-database'
-		dbPort    = mustGetenv("DB_PORT")       // e.g. '3306'
-		dbTCPHost = mustGetenv("INSTANCE_HOST") // e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
+		dbUser    = os.Getenv("DB_USER")       // e.g. 'my-db-user'
+		dbPwd     = os.Getenv("DB_PASS")       // e.g. 'my-db-password'
+		dbName    = os.Getenv("DB_NAME")       // e.g. 'my-database'
+		dbPort    = os.Getenv("DB_PORT")       // e.g. '3306'
+		dbTCPHost = os.Getenv("INSTANCE_HOST") // e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
 	)
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		dbUser, dbPwd, dbTCPHost, dbPort, dbName)
 
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// end connected
 
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	fmt.Println(db)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
