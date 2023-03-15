55d6c89c587e67531a2128b9d625cbb7d868ba0b
ahhzaky <siahmadzaky@gmail.com>
2023-03-15 14:57:57 +0700

    :memo: not check to db, and commit first

--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
-	fmt.Println("This is service user investor")
+	mustGetenv := func(k string) string {
+		v := os.Getenv(k)
+		if v == "" {
+			log.Fatalf("Fatal Error in connect_tcp.go: %s environment variable not set.", k)
+		}
+		return v
+	}
+	var (
+		dbUser    = mustGetenv("DB_USER")       // e.g. 'my-db-user'
+		dbPwd     = mustGetenv("DB_PASS")       // e.g. 'my-db-password'
+		dbName    = mustGetenv("DB_NAME")       // e.g. 'my-database'
+		dbPort    = mustGetenv("DB_PORT")       // e.g. '3306'
+		dbTCPHost = mustGetenv("INSTANCE_HOST") // e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
+	)
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		dbUser, dbPwd, dbTCPHost, dbPort, dbName)
+
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// SETUP REPO
+
+	// END SETUP
+
+	// RUN SERVICE
+	router := gin.Default()
+
+	api := router.Group("api/v1")
+
+	// handler start
+	api.GET("/initial")
+
+	// end handler
+	router.Run()
+
+	// fmt.Println("This is service user investor")
 }
