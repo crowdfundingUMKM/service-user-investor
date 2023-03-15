08b0a7d5fdd6394d34e09dcb608b09323300a1ac
ahhzaky <siahmadzaky@gmail.com>
2023-03-15 22:13:27 +0700

    :memo: fix input

--- investor/input.go
+++ investor/input.go
@@ -1,1 +1,17 @@
 package investor
+
+type RegisterUserInput struct {
+	Name     string `json:"name" binding:"required"`
+	Phone    int    `json:"phone" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+type LoginInput struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+type CheckEmailInput struct {
+	Email string `json:"email" binding:"required,email"`
+}
