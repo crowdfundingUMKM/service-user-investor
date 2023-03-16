fafd217d631d193799c1d2c65b98c9262ae21c0d
ahhzaky <siahmadzaky@gmail.com>
2023-03-16 13:14:55 +0700

    :tada: fixed database connection

--- main.go
+++ main.go
@@ -1,50 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"service-user-investor/auth"
+	"service-user-investor/database"
 	"service-user-investor/handler"
 	"service-user-investor/investor"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 func main() {
-	// initial connected db
+	// env
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
-	var (
-		dbUser    = os.Getenv("DB_USER")       // e.g. 'my-db-user'
-		dbPwd     = os.Getenv("DB_PASS")       // e.g. 'my-db-password'
-		dbName    = os.Getenv("DB_NAME")       // e.g. 'my-database'
-		dbPort    = os.Getenv("DB_PORT")       // e.g. '3306'
-		dbTCPHost = os.Getenv("INSTANCE_HOST") // e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
-	)
-
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		dbUser, dbPwd, dbTCPHost, dbPort, dbName)
-
-	// end connected
-
-	// open connection
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	fmt.Println(db)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
 	// SETUP REPO
-
+	db := database.NewConnectionDB()
 	userInvestorRepository := investor.NewRepository(db)
 
 	// SETUP SERVICE
