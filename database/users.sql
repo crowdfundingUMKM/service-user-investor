7198184e07f4fa766d051f9c84189222879c266b
ahhzaky <siahmadzaky@gmail.com>
2023-03-20 23:25:56 +0700

    update sql

--- database/users.sql
+++ database/users.sql
@@ -12,3 +12,12 @@ CREATE TABLE `users`
     `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- data
+INSERT INTO `users` (`id`, `name`, `email`, `phone`, `password_hash`, `avatar_file_name`, `role`, `token`, `created_at`, `updated_at`) VALUES
+(1, 'Ahmad Zaky', 'test@gmail.com', 875688222, '$2a$04$6A5/psA4hCa0p0mLZQw4A.GKrkYDH3nTiim8lj9mYS18dmVi2FIvO', '', 'activate', '', '2023-03-15 22:56:25', '2023-03-15 22:56:25');
+
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
